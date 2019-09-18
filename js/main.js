5df92d53153ad73637b9380336580d5b67c02154
Spence Nelson <spence.nelson@threespot.com>
2019-09-18 14:26:47 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
 
    $('#load-content').load("nav.htm");
 
+   $(".expanding-nav-overlay-button").click(function(){
+     $(".expanding-nav-overlay, .expanding-nav-overlay-button").toggleClass("nav-open");
+   });
+
 });
