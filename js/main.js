207645385fc673e2b659e5d8b37e7a9e6b03b8ac
Spence Nelson <spence.nelson@threespot.com>
2019-09-18 14:46:02 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-   $('#load-content').load("nav.htm");
+   $('#load-content').load("/gd5569/nav.htm");
 
    $(".expanding-nav-overlay-button").click(function(){
      $(".expanding-nav-overlay, .expanding-nav-overlay-button").toggleClass("nav-open");
